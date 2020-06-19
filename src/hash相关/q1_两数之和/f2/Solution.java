dfb1aa156dc01e846ea25e693e27b99a2c25da5a
youngMen1 <fengzhiqiangzxx199644>
2020-06-19 11:15:20 +0800

    update

--- src/hash相关/q1_两数之和/f2/Solution.java
+++ src/hash相关/q1_两数之和/f2/Solution.java
@@ -8,7 +8,7 @@ import java.util.Map;
  */
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>(16);
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
                 return new int[]{map.get(target - nums[i]), i};
