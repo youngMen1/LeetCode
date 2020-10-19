787eddfd905cbbe769e30b0208d479060dbab9e2
youngMen1 <fengzhiqiangzxx199644>
2020-10-19 15:42:58 +0800

    desc:王争数据结构和算法两数之和

--- src/hash相关/q1_两数之和/f1/Solution.java
+++ src/hash相关/q1_两数之和/f1/Solution.java
@@ -1,20 +1,28 @@
 package hash相关.q1_两数之和.f1;
 
+import java.util.Arrays;
+
 /**
  * 暴力法 o(n^2)
  */
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
                     result[0] = i;
                     result[1] = j;
+                    System.out.println(Arrays.toString(result));
                     return result;
                 }
             }
         }
         return null;
+    }
+
+    public static void main(String[] args) {
+        int data[] = new int[]{2, 7, 11, 15};
+        System.out.println(twoSum(data,9));
     }
 }
--- src/hash相关/q1_两数之和/f2/Solution.java
+++ src/hash相关/q1_两数之和/f2/Solution.java
@@ -1,5 +1,6 @@
 package hash相关.q1_两数之和.f2;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,7 +8,7 @@ import java.util.Map;
  * 一遍hash o(n)
  */
 public class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>(16);
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
@@ -16,5 +17,12 @@ public class Solution {
             map.put(nums[i], i);
         }
         return null;
+    }
+
+    public static void main(String[] args) {
+        int data[] = new int[]{2, 7, 11, 15};
+        // 请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+        int[] result = twoSum(data,9);
+        System.out.println(Arrays.toString(result));
     }
 }
