7b0cf7086d0079f708f690e71547e52e8211a44c
youngMen1 <fengzhiqiangzxx@163.com>
2021-01-05 17:53:39 +0800

    desc:王争数据结构和算法两数之和

--- src/hash相关/q1_两数之和/f1/Solution.java
+++ src/hash相关/q1_两数之和/f1/Solution.java
@@ -1,11 +1,31 @@
 package hash相关.q1_两数之和.f1;
 
+
 import java.util.Arrays;
 
 /**
- * 暴力法 o(n^2)
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 输入：l1 = [2,4,3], l2 = [5,6,4]
+ * 输出：[7,0,8]
+ * 解释：342 + 465 = 807.
  */
 class Solution {
+
+    public static void main(String[] args) {
+        int data[] = new int[]{2, 7, 11, 15};
+        System.out.println(twoSum(data, 9));
+    }
+
+    /**
+     * 暴力法 o(n^2)
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
     public static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
@@ -21,8 +41,5 @@ class Solution {
         return null;
     }
 
-    public static void main(String[] args) {
-        int data[] = new int[]{2, 7, 11, 15};
-        System.out.println(twoSum(data,9));
-    }
+
 }
